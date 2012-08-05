6206114dedfe0ca4e8bad7f91ffd25e266d63bae
Zoltán Nagy <abesto@abesto.net>
2012-08-05 23:38:20 +0200

    Use windows (well... only one, for now)

--- Main.hs
+++ Main.hs
@@ -7,78 +7,82 @@ import System.Random
 import Control.Monad (when, liftM)
 
 data BoardPosition = BoardPosition Int Int
-data ScreenPosition = ScreenPosition Int Int
+data WBoardPosition = WBoardPosition Int Int
+
+data UI = UI {
+      uiGame   :: Game
+    , uiWBoard :: Window
+    , uiStyles :: [CursesStyle]
+    }
 
 ---- Conversion between coordinate systems ----
 -- All systems are 0-based
 -- ModelPosition: row 0 is the first guess; can't index the code row (used in Model.hs)
 -- BoardPosition: row 0 is the code row; guess rows are indexed top-to-bottom
--- ScreenPosition: the raw character position on screen, as used by ncurses
-screenToBoard :: ScreenPosition -> BoardPosition
-screenToBoard (ScreenPosition y x) = BoardPosition
+-- WBoardPosition: the raw character position on the board window
+wBoardToBoard :: WBoardPosition -> BoardPosition
+wBoardToBoard (WBoardPosition y x) = BoardPosition
                                        ((y + 1) `div` 2 - 1)
                                        ((x + 1) `div` 2 - 1)
 
-boardToScreen :: BoardPosition -> ScreenPosition
-boardToScreen (BoardPosition y x) = ScreenPosition
+boardToWBoard :: BoardPosition -> WBoardPosition
+boardToWBoard (BoardPosition y x) = WBoardPosition
                                        (y * 2 + 1)
                                        (x * 2 + 1)
 
-boardToModel :: Game -> BoardPosition -> ModelPosition
-boardToModel game (BoardPosition y x) = ModelPosition (rowCount game - y) x
+boardToModel :: UI -> BoardPosition -> ModelPosition
+boardToModel UI {uiGame=game} (BoardPosition y x) = ModelPosition (rowCount game - y) x
 
-modelToBoard :: Game -> ModelPosition -> BoardPosition
-modelToBoard game (ModelPosition y x) = BoardPosition (rowCount game - y) x
+modelToBoard :: UI -> ModelPosition -> BoardPosition
+modelToBoard UI {uiGame=game} (ModelPosition y x) = BoardPosition (rowCount game - y) x
 
-screenToModel :: Game -> ScreenPosition -> ModelPosition
-screenToModel game = boardToModel game . screenToBoard
+screenToModel :: UI -> WBoardPosition -> ModelPosition
+screenToModel ui = boardToModel ui . wBoardToBoard
 
-modelToScreen :: Game -> ModelPosition -> ScreenPosition
-modelToScreen game = boardToScreen . modelToBoard game
+modelToScreen :: UI -> ModelPosition -> WBoardPosition
+modelToScreen ui = boardToWBoard . modelToBoard ui
 
 
 ---- Drawing functions ----
-putCh :: Char -> ScreenPosition -> IO ()
-putCh c (ScreenPosition y x) = mvAddCh y x $ toEnum $ fromEnum c
+putCh :: Char -> UI -> WBoardPosition -> IO ()
+putCh c (UI {uiWBoard=wBoard}) (WBoardPosition y x) = mvWAddStr wBoard y x [c]
 
-drawBox :: (Int, Int) -> (Int, Int) -> IO ()
-drawBox (y0, x0) (y1, x1) = do
-  mapM_ (putCh '|') [ScreenPosition y x | y <- [y0+1 .. y1-1], x <- [x0, x1]]
-  mapM_ (putCh '-') [ScreenPosition y x | y <- [y0, y1], x <- [x0+1 .. x1]]
-  mapM_ (putCh '+') [ScreenPosition y x | y <- [y0, y1], x <- [x0, x1]]
+drawBox :: UI -> (Int, Int) -> (Int, Int) -> IO ()
+drawBox ui (y0, x0) (y1, x1) = do
+  mapM_ (putCh '|' ui) [WBoardPosition y x | y <- [y0+1 .. y1-1], x <- [x0, x1]]
+  mapM_ (putCh '-' ui) [WBoardPosition y x | y <- [y0, y1], x <- [x0+1 .. x1]]
+  mapM_ (putCh '+' ui) [WBoardPosition y x | y <- [y0, y1], x <- [x0, x1]]
 
-drawCodePeg :: [CursesStyle] -> CodePeg -> IO ()
+drawCodePeg :: UI -> CodePeg -> IO ()
 drawCodePeg _ Empty = return ()
-drawCodePeg styles p = do
-  setStyle $ styles !! fromEnum p
-  wAddStr stdScr $ show $ fromEnum p
-  resetStyle
+drawCodePeg UI {uiStyles=styles, uiWBoard=wBoard} p =
+    wWithStyle wBoard (styles !! fromEnum p) (wAddStr wBoard $ show $ fromEnum p)
 
-showCode :: [CursesStyle] -> Game -> IO ()
-showCode styles game = mapM_ f $ zip [0..] (codePegs $ code game)
+showCode :: UI -> IO ()
+showCode ui@(UI {uiGame=game}) = mapM_ f $ zip [0..] (codePegs $ code game)
     where f (n, p) = do
-            moveBoard $ BoardPosition 0 n
-            drawCodePeg styles p
+            moveBoard ui $ BoardPosition 0 n
+            drawCodePeg ui p
 
-drawBoard :: Game -> IO ()
-drawBoard game = do
+drawBoard :: UI -> IO ()
+drawBoard ui@(UI {uiGame=game}) = do
   erase
-  mapM_ (uncurry drawBox) [
+  mapM_ (uncurry $ drawBox ui) [
                          ((2*row, 2*column), (2*(row+1), 2*(column+1))) |
                          row <- [0 .. rowCount game], column <- [0 .. pegCount game - 1]
                         ]
-  mapM_ (putCh '?') [boardToScreen $ BoardPosition 0 x | x <- [0 .. pegCount game - 1]]
+  mapM_ (putCh '?' ui) [boardToWBoard $ BoardPosition 0 x | x <- [0 .. pegCount game - 1]]
 
 -- Position where the number of black key pegs begins
 -- the row parameter is in the BoardPosition coordinate system
-blacksPosition :: Game -> Int -> ScreenPosition
+blacksPosition :: Game -> Int -> WBoardPosition
 blacksPosition game row =
     let f (BoardPosition r c) = BoardPosition r (c+2) in
-    boardToScreen $ f $ BoardPosition row (pegCount game - 1)
+    boardToWBoard $ f $ BoardPosition row (pegCount game - 1)
 
-whitesPosition :: Game -> Int -> ScreenPosition
+whitesPosition :: Game -> Int -> WBoardPosition
 whitesPosition game row =
-    let f (ScreenPosition r c) = ScreenPosition r (c+10) in
+    let f (WBoardPosition r c) = WBoardPosition r (c+10) in
     f $ blacksPosition game row
 
 debug :: Show a => a -> IO ()
@@ -90,89 +94,98 @@ debug game = do
 
 
 ---- Navigation ----
-getYXScreen :: IO ScreenPosition
-getYXScreen = do
-  (y, x) <- getYX stdScr
-  return $ ScreenPosition y x
+getYXwBoard :: UI -> IO WBoardPosition
+getYXwBoard ui = uncurry WBoardPosition `liftM` (getYX $ uiWBoard ui)
 
-moveScreen :: ScreenPosition -> IO ()
-moveScreen (ScreenPosition y x) = move y x
+moveWBoard :: UI -> WBoardPosition -> IO ()
+moveWBoard ui (WBoardPosition y x) = wMove (uiWBoard ui) y x
 
-getYXBoard :: IO BoardPosition
-getYXBoard = screenToBoard `liftM` getYXScreen
+getYXBoard :: UI -> IO BoardPosition
+getYXBoard ui = wBoardToBoard `liftM` getYXwBoard ui
 
-moveBoard :: BoardPosition -> IO ()
-moveBoard = moveScreen . boardToScreen
+moveBoard :: UI -> BoardPosition -> IO ()
+moveBoard ui = moveWBoard ui . boardToWBoard
 
-getYXModel :: Game -> IO ModelPosition
-getYXModel game = screenToModel game `liftM` getYXScreen
+getYXModel :: UI -> IO ModelPosition
+getYXModel ui = screenToModel ui `liftM` getYXwBoard ui
 
-moveModel :: Game -> ModelPosition -> IO ()
-moveModel game = moveScreen . modelToScreen game
+moveModel :: UI -> ModelPosition -> IO ()
+moveModel ui = moveWBoard ui . modelToScreen ui
 
 
 ---- Handle user input ----
-handleInput :: [CursesStyle] -> Game -> IO ()
-handleInput styles game = let sizeX = pegCount game in do
+handleInput :: UI -> IO ()
+handleInput ui = let sizeX = pegCount (uiGame ui) in do
   --debug game
-  refresh
+  wRefresh (uiWBoard ui)
   c <- getCh
-  boardPos@(BoardPosition y x) <- getYXBoard
+  boardPos@(BoardPosition y x) <- getYXBoard ui
   case c of
     KeyLeft  -> do
-           when (x > 0) $ moveBoard $ BoardPosition y (x-1)
-           handleInput styles game
+           when (x > 0) $ moveBoard ui $ BoardPosition y (x-1)
+           handleInput ui
     KeyRight -> do
-           when (x < (sizeX-1)) $ moveBoard $ BoardPosition y (x+1)
-           handleInput styles game
+           when (x < (sizeX-1)) $ moveBoard ui $ BoardPosition y (x+1)
+           handleInput ui
     KeyChar 'g' ->
-        let game' = guess game in
+        let ui'@(UI {uiGame=game'}) = ui {uiGame = guess (uiGame ui)} in
         do
           when (outcome game' /= Playing) $ do
-              showCode styles game'
-              moveBoard boardPos
+              showCode ui'
+              moveBoard ui boardPos
           when (all (== Empty) (guessPegs $ currentGuess game')) $ do
-              moveScreen $ blacksPosition game' y
-              wAddStr stdScr $ show $ length $ filter (== Black) $ keyPegs $ lastResult game'
-              moveScreen $ whitesPosition game' y
-              wAddStr stdScr $ show $ length $ filter (== White) $ keyPegs $ lastResult game'
-              moveModel game' $ ModelPosition (length (guesses game') - 1) x
-          handleInput styles game'
+              moveWBoard ui' $ blacksPosition game' y
+              wAddStr (uiWBoard ui) $ show $ length $ filter (== Black) $ keyPegs $ lastResult game'
+              moveWBoard ui' $ whitesPosition game' y
+              wAddStr (uiWBoard ui) $ show $ length $ filter (== White) $ keyPegs $ lastResult game'
+              moveModel ui' $ ModelPosition (length (guesses game') - 1) x
+          handleInput ui'
     KeyChar 'q' ->
         return ()
     KeyChar k ->
         let
             f [(i, "")] =
-                  let game' = modifyGuess game x (toEnum i) in do
-                    drawCodePeg styles $ toEnum $ fromEnum $ pegAt game' $ boardToModel game' boardPos
-                    moveBoard boardPos
-                    handleInput styles game'
-            f _ = handleInput styles game
+                  let ui'@(UI {uiGame=game'}) = ui {uiGame = modifyGuess (uiGame ui) x (toEnum i)} in do
+                    drawCodePeg ui' $ toEnum $ fromEnum $ pegAt game' $ boardToModel ui' boardPos
+                    moveBoard ui boardPos
+                    handleInput ui'
+            f _ = handleInput ui
         in f $ reads [k]
-    _ -> handleInput styles game
+    _ -> handleInput ui
 
 
 -- Startup
 main :: IO ()
-main = do
-  game <- generateGame 10 4 6 `liftM` getStdGen
+main = let rows = 10
+           pegs = 4
+           colors = 6
+           (WBoardPosition boardLines boardColumns) = boardToWBoard $ BoardPosition rows (pegs - 1) in do
   start
   keypad stdScr True
   echo False
 
+  game <- generateGame rows pegs colors `liftM` getStdGen
   styles <- convertStyles [Style c BlackB | c <- [BlackF, RedF, GreenF, BlueF, YellowF, MagentaF, CyanF]]
+  wBoard <- newWin (boardLines + 10) (boardColumns + 20) 0 0
 
-  drawBoard game
+  let ui = UI {
+             uiGame = game
+           , uiWBoard = wBoard
+           , uiStyles = styles
+           } in do
 
-  moveScreen $ blacksPosition game 0
-  wAddStr stdScr "Blacks"
 
-  moveScreen $ whitesPosition game 0
-  wAddStr stdScr "Whites"
+      drawBoard ui
 
-  moveBoard $ modelToBoard game (ModelPosition 0 0)
-  refresh
-  handleInput styles game
+      let (WBoardPosition y x) = blacksPosition game 0 in
+        mvWAddStr (uiWBoard ui) y x "Blacks"
 
-  echo True
-  end
+      let (WBoardPosition y x) = whitesPosition game 0 in
+        mvWAddStr (uiWBoard ui) y x "Whites"
+
+      moveBoard ui $ modelToBoard ui (ModelPosition 0 0)
+      refresh
+      handleInput ui
+
+      echo True
+      end
--- README.md
+++ README.md
@@ -3,6 +3,8 @@ hs-mastermind
 
 ncurses-based mastermind in haskell
 
+Uses functionality from the hscurses branch at https://github.com/abesto/hscurses. After I get the go-ahead for some of the stuff in it I found online, I'll submit a pull request and it will hopefully be available via Cabal. Until then, you have to install it manually; sorry.
+
 run with
 
     cabal build && ./dist/build/main/main
